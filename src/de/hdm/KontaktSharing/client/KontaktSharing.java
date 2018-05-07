1f5ecb45650d1609b06bd0993d0815da4c42d1c5
Jiayi Li <jiayi.li@zoi.de>
2018-05-07 20:20:42 +0200

    Project is running now

---
+++ src/de/hdm/KontaktSharing/client/KontaktSharing.java
@@ -0,0 +1,13 @@
+package de.hdm.KontaktSharing.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class KontaktSharing implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
