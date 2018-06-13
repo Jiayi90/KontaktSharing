213683b3b9774c60c93d6960271e80b17feda874
SaminaAhmed <sa056@hdm-stuttgart.de>
2018-06-13 09:55:03 +0200

    LoginServiceImpl klasse im server package ergänzt

--- src/de/hdm/KontaktSharing/server/LoginServiceImpl.java
+++ src/de/hdm/KontaktSharing/server/LoginServiceImpl.java
@@ -7,24 +7,30 @@ import de.hdm.KontaktSharing.shared.bo.LoginInfo;
 import de.hdm.KontaktSharing.shared.LoginService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
-public class LoginServiceImpl extends RemoteServiceServlet implements
-    LoginService {
+public class LoginServiceImpl extends RemoteServiceServlet implements LoginService {
 
-  public LoginInfo login(String requestUri) {
-    UserService userService = UserServiceFactory.getUserService();
-    User user = userService.getCurrentUser();
-    LoginInfo loginInfo = new LoginInfo();
+	/**
+	 * Dient zum Serialisieren von Objekten fuer eine RPC faehigen austausch
+	 * zwischen Server und Client.
+	 */
 
-    if (user != null) {
-      loginInfo.setLoggedIn(true);
-      loginInfo.setEmailAddress(user.getEmail());
-      loginInfo.setNickname(user.getNickname());
-      loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
-    } else {
-      loginInfo.setLoggedIn(false);
-      loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
-    }
-    return loginInfo;
-  }
+	private static final long serialVersionUID = 1L;
+
+	public LoginInfo login(String requestUri) {
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		LoginInfo loginInfo = new LoginInfo();
+
+		if (user != null) {
+			loginInfo.setLoggedIn(true);
+			loginInfo.setEmailAddress(user.getEmail());
+			loginInfo.setNickname(user.getNickname());
+			loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
+		} else {
+			loginInfo.setLoggedIn(false);
+			loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
+		}
+		return loginInfo;
+	}
 
 }
