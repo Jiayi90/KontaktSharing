f9bce5bf5f79153759a1fcfd643df2ccbffe0973
SaminaAhmed <sa056@hdm-stuttgart.de>
2018-06-28 19:54:13 +0200

    KontaktSharing Klasse mit Login

--- src/de/hdm/KontaktSharing/client/KontaktSharing.java
+++ src/de/hdm/KontaktSharing/client/KontaktSharing.java
@@ -4,231 +4,231 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
-
-import de.hdm.KontaktSharing.client.page.CommonPage;
-import de.hdm.KontaktSharing.client.page.DemoPage;
-import de.hdm.KontaktSharing.client.widget.ContentWidget;
-import de.hdm.KontaktSharing.client.widget.HeaderWidget;
-import de.hdm.KontaktSharing.client.widget.NavigationWidget;
-import de.hdm.KontaktSharing.shared.KontaktSharingAdministration;
-import de.hdm.KontaktSharing.shared.KontaktSharingAdministrationAsync;
-import de.hdm.KontaktSharing.shared.LoginService;
-import de.hdm.KontaktSharing.shared.LoginServiceAsync;
-import de.hdm.KontaktSharing.shared.bo.LoginInfo;
-import de.hdm.KontaktSharing.shared.bo.Nutzer;
-
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import de.hdm.KontaktSharing.client.page.*;
+import de.hdm.KontaktSharing.client.widget.*;
+import de.hdm.KontaktSharing.shared.*;
+import de.hdm.KontaktSharing.shared.LoginService;
+import de.hdm.KontaktSharing.shared.LoginServiceAsync;
+import de.hdm.KontaktSharing.shared.bo.*;
+import de.hdm.KontaktSharing.shared.report.AllKontaktReport;
+
+
+
 
 //@author samina
 
 public class KontaktSharing implements EntryPoint {
 
-	// /**
-	// * The message displayed to the user when the server cannot be reached or
-	// * returns an error.
-	// */
-	//
-	// private static final String SERVER_ERROR = "An error occurred while"
-	// + "attempting to contact the server. Please check your network" + "connection
-	// and try again.";
-	//
-	// /**
-	// * Create a remote service proxy to talk to the server-side Greeting service.
-	// *
-	// */
-	// private final KontaktSharingAdministrationAsync projektService =
-	// GWT.create(KontaktSharingAdministration.class);
-	//
-
-	/**
-	 * Panel, Label und Anchor für die Anmeldung
-	 */
 	private LoginInfo loginInfo = null;
 	private VerticalPanel loginPanel = new VerticalPanel();
-	private Label loginLabel = new Label(
-			"Please sign in to your Google Account to access the StockWatcher application.");
+	private Label loginMessage = new Label("Bitte logge dich mit dem Google Account ein");
 	private Anchor signInLink = new Anchor("Sign In");
 	private Anchor signOutLink = new Anchor("Sign Out");
-
+	
 	private static KontaktSharingAdministrationAsync ksverwaltung = ClientsideSettings
 			.getKontaktSharingAdministration();
-	/**
-	 * Anlegen vom report und anchor
-	 */
-	private Button zumreportgenerator = new Button("Zum Report Generator");
-	private Anchor reportLink = new Anchor();
-	private Button zuKontaktsharing = new Button("Zu Kontaktsharing");
+	
+	private TextBox textBox = new TextBox();
+	private Button suchenButton = new Button("Detailsuche");
+	private Button logoutButton = new Button("Ausloggen");
+	private Button loginButton = new Button("LOGIN");
 
-	/**
-	 * Hier steigt das Programm beim Start ein
-	 */
+	@Override
 	public void onModuleLoad() {
-
-		
-		//loadPanel();
-		// Check login status using login service.
 		LoginServiceAsync loginService = GWT.create(LoginService.class);
-		// Start-URL der Anwendung GWT.getHostPageBaseURL() !!!
-		loginService.login(GWT.getHostPageBaseURL() + "KontaktSharing.html", new AsyncCallback<LoginInfo>() {
+		loginService.login(GWT.getHostPageBaseURL() + "KontaktSharing.html", new LoginCallback());
 
-			/**
-			 * siehe client google report
-			 */
-			public void onFailure(Throwable caught) {
-				Window.alert("Fehler Login: " + caught.toString());
-			}
-
-			/**
-			 * siehe client google report
-			 */
-			public void onSuccess(LoginInfo result) {
-				loginInfo = result;
-				if (loginInfo.isLoggedIn()) {
-
-					ksverwaltung.checkNutzer(loginInfo.getEmailAddress(), new AsyncCallback<Nutzer>() {
-
-						public void onFailure(Throwable caught) {
-							// TODO Auto-generated method stub
-							Window.alert("Fehler beim Laden: " + caught.getLocalizedMessage());
-						}
-
-						public void onSuccess(Nutzer result) {
-							// Ueberpruefung, ob Nutzer bereits existiert.
-							if (result != null) {
-								RootPanel.get("Details").clear();
-								loadKontaktSharing(result);
-							} else {
-								RootPanel.get("Details").clear();
-
-							}
-
-						}
-
-					});
-
-				} else {
-					loadLogin();
-				}
-
-			}
-
-		});
 	}
-
-	/**
-	 * Zusammenstellung des Anmeldeabschnitts
-	 */
+	
 	private void loadLogin() {
-		zuKontaktsharing.addClickHandler(new ClickHandler() {
+
+		loginButton.addClickHandler(new loginButtonClickHandler());
+		loginButton.setStylePrimaryName("loginButton");
+		Widget welcomeMessage = null;
+		welcomeMessage.setStylePrimaryName("landingPageWelcomeMessage");
+		loginPanel.setStylePrimaryName("loginPanel");
+		loginMessage.setStylePrimaryName("landingPageLoginMessage");
+		loginPanel.add(welcomeMessage);
+		loginPanel.add(loginMessage);
+		loginPanel.add(loginButton);
+		RootPanel.get("content").add(loginPanel);
+	}
+	
+	private void loadKontaktSharing() {
+		textBox.addKeyPressHandler(new KeyPressHandler() {
+
 			@Override
-			public void onClick(ClickEvent event) {
-
-				signInLink.setHref(loginInfo.getLoginUrl());
-				Window.open(signInLink.getHref(), "_self", "");
-
+			public void onKeyPress(KeyPressEvent event) {
+				if (event.getNativeEvent().getKeyCode() == KeyCodes.KEY_ENTER) {
+					Widget panelSuche = new Widget();
+					RootPanel.get("content").clear();
+					RootPanel.get("content").add(panelSuche);
+					textBox.setText("");
+				}
 			}
 		});
-		zumreportgenerator.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				reportLink.setHref(GWT.getHostPageBaseURL() + "KontaktSharingReport.html");
-				Window.open(reportLink.getHref(), "_self", "");
-			}
-		});
-		loginPanel.add(loginLabel);
-		loginPanel.add(zuKontaktsharing);
-		loginPanel.add(zumreportgenerator);
-		RootPanel.get("Details").add(loginPanel);
+		
+		HorizontalPanel flowpanel = new HorizontalPanel();
+		// Cookies.setCookie("logout", loginInfo.getLogoutUrl());
+		textBox.setStylePrimaryName("searchTextBox");
+		textBox.setMaxLength(100);
+		textBox.getElement().setPropertyString("placeholder", " Schnellsuche...");
+		flowpanel.add(textBox);
+		flowpanel.setStylePrimaryName("logoutBarContainer");
+		RootPanel.get("logout").add(flowpanel);// hPanelBar.add(logoutButton);
+		RootPanel.get("leftmenutree").clear();
+		AllKontaktReport kontaktView = new AllKontaktReport();
+
+		// AUFRUF DES BAUMS
+//		CustomTreeModel ctm = new CustomTreeModel();
+//		RootPanel.get("leftmenutree").clear();
+//		RootPanel.get("leftmenutree").add(ctm);
+//
+//		signOutLink.setHref(loginInfo.getLogoutUrl());
+//
+//		Menubar mb = new Menubar();
+	}
+	class loginButtonClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			signInLink.setHref(loginInfo.getLoginUrl());
+			Window.open(signInLink.getHref(), "_self", "");
+		}
 
 	}
 
-	/**
-	 * laden des KontaktSharing. (Startseite Menübar)
-	 * 
-	 * @param profil
-	 */
-	private void loadKontaktSharing(final Nutzer nutzer) {
-		signOutLink.setHref(loginInfo.getLogoutUrl());
-		RootPanel.get("Topbar").add(signOutLink);
-		RootPanel.get("Details").add(new ContentWidget(nutzer));
-		RootPanel.get("Navigator").add(new NavigationWidget(loginPanel, null));
+	class LoginCallback implements AsyncCallback<LoginInfo> {
 
+		@Override
+		public void onFailure(Throwable caught) {
+			Window.alert("Fehler beim Login: " + caught.getMessage());
+		}
+
+		@Override
+		public void onSuccess(LoginInfo result) {
+			loginInfo = result;
+			if (loginInfo.isLoggedIn()) {
+				ksverwaltung.checkNutzer(loginInfo.getEmailAddress(), new FindNutzerCallback());
+
+			} else {
+				loadLogin();
+			}
+		}
+
+	}
+
+	class FindNutzerCallback implements AsyncCallback<Nutzer> {
+
+		@Override
+		public void onFailure(Throwable caught) {
+			Window.alert("Fehler beim Login: " + caught.getMessage());
+		}
+
+		@Override
+		public void onSuccess(Nutzer result) {
+			if (result != null) {
+				Cookies.setCookie("email", result.getEmail());
+				Cookies.setCookie("id", result.getId() + "");
+				loadKontaktSharing();
+			} else {
+				CreateNutzerDialogBox dialogbox = new CreateNutzerDialogBox(loginInfo.getEmailAddress());
+				dialogbox.center();
+
+			}
+		}
+
+	}
+
+	class CreateNutzerDialogBox extends DialogBox {
+		private Label frage = new Label(
+				"Sie haben noch keinen Nutzer auf diesem Kontaktmanager. MÃ¶chten Sie einen neuen Nutzer anlegen?");
+		private Button ja = new Button("Ja");
+		private Button nein = new Button("Nein");
+		private String googleMail = "";
+		private VerticalPanel vpanel = new VerticalPanel();
+
+		public CreateNutzerDialogBox(String mail) {
+			googleMail = mail;
+			ja.addClickHandler(new CreateNutzerClickHandler());
+			nein.addClickHandler(new DontCreateNutzerClickHandler());
+			vpanel.add(frage);
+			vpanel.add(ja);
+			vpanel.add(nein);
+			this.add(vpanel);
+
+		}
+
+		class CreateNutzerCallback implements AsyncCallback<Nutzer> {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("Ihr User konnte nicht erstellt werden" + caught.getMessage());
+			}
+
+			@Override
+			public void onSuccess(Nutzer result) {
+				Window.alert("Ihr Nutzer wurde erfolgreich angelegt");
+				Cookies.setCookie("email", result.getEmail());
+				Cookies.setCookie("id", result.getId() + "");
+				loadKontaktSharing();
+				hide();
+			}
+
+		}
+
+		class CreateNutzerClickHandler implements ClickHandler {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				ksverwaltung.checkNutzer(googleMail, new CreateNutzerCallback());
+
+			}
+
+		}
+
+		class DontCreateNutzerClickHandler implements ClickHandler {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				hide();
+				signOutLink.setHref(loginInfo.getLogoutUrl());
+				Window.open(signOutLink.getHref(), "_self", "");
+
+			}
+
+		}
+	}
+
+	public static class SuchenCommand implements Command {
+
+		@Override
+		public void execute() {
+			Widget panelSuche = new Widget();
+			RootPanel.get("content").clear();
+			RootPanel.get("content").add(panelSuche);
+		}
 	}
 
 }
 
-// VerticalPanel vpNavigation = new VerticalPanel();
-// Button btStartseite = new Button("Startseite");
-// Button btPost = new Button("Post");
-// Button btImpressum = new Button("Impressum");
-// Button btAusloggen = new Button("Ausloggen");
-//
-// public void onModuleLoad() {
-//
-// // Check login status using login service.
-// LoginServiceAsync loginService = GWT.create(LoginService.class);
-//// loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>()
-// {
-//// public void onFailure(Throwable error) {
-//// }
-////
-//// public void onSuccess(LoginInfo result) {
-//// loginInfo = result;
-//// if (loginInfo.isLoggedIn()) {
-//// loadPanel();
-//// } else {
-//// loadLogin();
-//// }
-////
-//// }
-//// });
-//
-// Cookies.setCookie("email", "hdm@hdm4.de");
-//
-// loadGUI();
-//
-//
-// }
-//
-// private void loadGUI() {
-//
-// }
-//
-// private void loadLogin() {
-// // Assemble login panel.
-// signInLink.setHref(loginInfo.getLoginUrl());
-// loginPanel.add(loginLabel);
-// loginPanel.add(signInLink);
-// RootPanel.get("rootElement").add(loginPanel);
-// }
-//
-// private void loadPanel() {
-// VerticalPanel rootPanel = new VerticalPanel();
-//
-// HorizontalPanel mainPanel = new HorizontalPanel();
-// mainPanel.getElement().setId("main");
-//
-// CommonPage contentPanel = new DemoPage();
-// HeaderWidget headerPanel = new HeaderWidget();
-// NavigationWidget navPanel = new NavigationWidget(mainPanel, contentPanel);
-//
-// rootPanel.add(headerPanel);
-// rootPanel.add(mainPanel);
-// mainPanel.add(navPanel);
-// mainPanel.add(contentPanel);
-//
-// RootPanel.get("rootElement").add(rootPanel);
-//
-// }
-//
-// }
+
+	
